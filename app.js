3e49764bc1a2c79a15469dd5ce3e5c4251fd3a4d
CandeCMS <candemacarena8@gmail.com>
2026-04-05 09:35:13 -0300

    Agregue 1.b y 1.c

--- app.js
+++ app.js
@@ -27,4 +27,48 @@ async function mostrarTodosLosPersonajes() {
 
 mostrarTodosLosPersonajes();
 
-//1.b
+//1.b Agregar un nuevo personaje (POST). 
+async function agregarPersonaje() {
+  try {
+    const nuevoPersonaje = {
+      id: 55,
+      firstName: "Night",
+      lastName: "King",
+      fullName: "Night King",
+      title: "Leader of the White Walkers",
+      family: "White Walkers",
+      image: "night-king.jpg",
+      imageUrl: "https://thronesapi.com/assets/images/night-king.jpg"
+    };
+
+    const respuesta = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(nuevoPersonaje)
+    });
+
+    const resultado = await respuesta.json();
+    console.log("1.b agregar personaje ");
+    console.log(resultado);
+  } catch (error) {
+    console.error("error al agregar personaje: ", error.message);
+  }
+}
+agregarPersonaje();
+
+
+//1.c Buscar la información de un determinado personaje, utilizando un “id” como parámetro (GET).
+async function buscarPersonajePorId(id) {
+  try {
+    const respuesta = await fetch(`${url}/${id}`);
+    const personaje = await respuesta.json();
+    console.log("1.c personaje por ID");
+    console.log(personaje);
+  } catch (error) {
+    console.error("error al buscar personaje: ", error.message);
+  }
+}
+
+buscarPersonajePorId(55); // ejemplo de que personaje busca
